b83127a6f4680ae467b44907e763ad6a1278bcb6
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-03-29 11:33:22 +0100

    logo href updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -85,7 +85,7 @@ const Navbar: React.FC = () => {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
